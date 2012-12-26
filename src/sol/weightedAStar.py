ca7cbb632e0eba987a521127fb08bcdae8b773be
galcohen1 <galcohen1@gmail.com>
2012-12-26 18:09:01 +0200

    added a file for WeightedAStar

---
+++ src/sol/weightedAStar.py
@@ -0,0 +1,17 @@
+'''
+Created on 2012-12-26
+
+@author: Gal
+'''
+
+class WeigthedAStar(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
